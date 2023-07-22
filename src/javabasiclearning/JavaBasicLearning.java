d87b340876aeb053e92a3e0d40dcbf2912a5adc0
lukas <lukas@192.168.0.206>
2023-07-22 23:04:35 +0200

    small-update-to-check-connection

--- src/javabasiclearning/JavaBasicLearning.java
+++ src/javabasiclearning/JavaBasicLearning.java
@@ -14,7 +14,7 @@ public class JavaBasicLearning {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World updated");
     }
     
 }
